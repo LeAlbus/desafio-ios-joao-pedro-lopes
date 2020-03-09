b9732f95630334c3260fe1d6f218a425b630edc1
Pedro Lopes <>
2020-03-08 21:00:12 -0300

    Added project, Pods and Gitignore

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'desafio-ios-pedro-lopes' do
+  	use_frameworks!
+	pod 'Alamofire', '~> 5.0'
+
+	target 'desafio-ios-pedro-lopesTests' do
+    		inherit! :search_paths
+    	# Pods for testing
+  	end
+end
---
+++ desafio-ios-pedro-lopes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  desafio-ios-pedro-lopes
+//
+//  Created by Pedro Lopes on 08/03/20.
+//  Copyright © 2020 Pedro Lopes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
